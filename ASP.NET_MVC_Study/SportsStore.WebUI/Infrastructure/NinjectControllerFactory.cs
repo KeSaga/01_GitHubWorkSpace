b13042edda130e715b4497ee20ed15f932c6d5d8
KeSaga <beijifeng_saga@163.com>
2015-05-25 10:01:58 +0000

    添加 MVC 真实案例的学习记录

---
+++ ASP.NET_MVC_Study/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using System.Web.Routing;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    /// <summary>
+    /// Ninjiect 控制器工厂类
+    /// </summary>
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel _ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            _ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)_ninjectKernel.Get(controllerType);
+        }
+
+        protected void AddBindings()
+        {
+            // 在这里设置绑定
+        }
+
+
+    }
+}
