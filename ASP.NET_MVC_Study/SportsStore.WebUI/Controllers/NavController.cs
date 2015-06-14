e00c1acc1b39c20b2fb5708381cffc8519ad117f
KeSaga <beijifeng_saga@163.com>
2015-06-14 16:02:58 +0800

    创建导航控制器

---
+++ ASP.NET_MVC_Study/SportsStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        //
+        // GET: /Nav/
+
+        public string Menu()
+        {
+            return "Hello from NavController";
+        }
+
+    }
+}
