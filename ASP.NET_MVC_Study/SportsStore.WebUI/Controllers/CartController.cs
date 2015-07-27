3746c76d84282ba2c160c2f61de61be3708cc95c
KeSaga <beijifeng_saga@163.com>
2015-07-27 22:29:08 +0800

    实现购物车控制器

---
+++ ASP.NET_MVC_Study/SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository _repository;
+
+        public CartController(IProductRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = _repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId,string returnUrl)
+        {
+            Product product = _repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            // 运用 ASP.NET 会话（Session）状态特性来存储和接收 Cart 对象
+            // Session 状态对象默认存储在 ASP.NET 服务器的内存中，但可以配置不同的存储方式，包括使用一个 SQL 数据库。
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+
+    }
+}
