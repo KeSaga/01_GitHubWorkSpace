09638efa8163cb8e5234dff25225a54b62f4c695
KeSaga <beijifeng_saga@163.com>
2015-07-31 22:53:17 +0800

    显示购物车内容

--- ASP.NET_MVC_Study/SportsStore.WebUI/Controllers/CartController.cs
+++ ASP.NET_MVC_Study/SportsStore.WebUI/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,15 @@ namespace SportsStore.WebUI.Controllers
             _repository = repo;
         }
 
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
         public RedirectToRouteResult AddToCart(int productId, string returnUrl)
         {
             Product product = _repository.Products
@@ -26,10 +36,13 @@ namespace SportsStore.WebUI.Controllers
             {
                 GetCart().AddItem(product, 1);
             }
+            // ------------注意：------------------
+            // 此方法下面和 RemoveFromCart 方法中使用的 RedirectToAction 方法的效果是
+            // 把一个 HTTP 的重定向指令发送到客户端浏览器，要求浏览器请求一个新的 URL。
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        public RedirectToRouteResult RemoveFromCart(int productId,string returnUrl)
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
         {
             Product product = _repository.Products
                 .FirstOrDefault(p => p.ProductID == productId);
