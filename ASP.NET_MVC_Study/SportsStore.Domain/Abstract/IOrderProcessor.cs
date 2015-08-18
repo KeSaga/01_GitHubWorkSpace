a375b4b065b616b576d834e60df439df066d2795
KeSaga <beijifeng_saga@163.com>
2015-08-18 17:41:18 +0800

    递交订单——实现订单处理器

---
+++ ASP.NET_MVC_Study/SportsStore.Domain/Abstract/IOrderProcessor.cs
@@ -0,0 +1,17 @@
+﻿using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Abstract
+{
+    /// <summary>
+    /// 订单处理接口
+    /// </summary>
+    public interface IOrderProcessor
+    {
+        void ProcessOrder(Cart cart, ShippingDetails shippingDetails);
+    }
+}
---
+++ ASP.NET_MVC_Study/SportsStore.Domain/Concrete/EmailOrderProcessor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Abstract;
+using System.Net.Mail;
+using System.Net;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "orders@example.com";
+        public string MailFromAddress = "sportsstore@example.com";
+        public bool UseSsl = true;
+        public string Username = "MySmtpUsername";
+        public string Password = "MySmtpPassword";
+        public string ServerName = "smtp.example.com";
+        public int ServerPort = 587;
+        public bool WriteAsFile = false;
+        public string FileLocation = @"c:\sports_store_emails";
+    }
+
+    public class EmailOrderProcessor : IOrderProcessor
+    {
+        private EmailSettings _emailSettings;
+
+        public EmailOrderProcessor(EmailSettings settings)
+        {
+            this._emailSettings = settings;
+        }
+
+        public void ProcessOrder(Cart cart, ShippingDetails shippingInfo)
+        {
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.EnableSsl = this._emailSettings.UseSsl;
+                smtpClient.Host = this._emailSettings.ServerName;
+                smtpClient.Port = this._emailSettings.ServerPort;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials =
+                    new NetworkCredential(this._emailSettings.Username, this._emailSettings.Password);
+
+                if (this._emailSettings.WriteAsFile)
+                {
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtpClient.PickupDirectoryLocation = this._emailSettings.FileLocation;
+                    smtpClient.EnableSsl = false;
+                }
+
+                StringBuilder body = new StringBuilder()
+                .AppendLine("A new order has been submitted")
+                .AppendLine("---")
+                .AppendLine("Items:");
+
+                foreach (var line in cart.Lines)
+                {
+                    var subtotal = line.Product.Price * line.Quantity;
+                    body.AppendFormat("{0} x {1} (subtotal: {2:c}", line.Quantity, line.Product.Name, subtotal);
+                }
+
+                body.AppendFormat("Total order value:{0:c}", cart.ComputeTotalValue())
+                    .AppendLine("---")
+                    .AppendLine("Ship to:")
+                    .AppendLine(shippingInfo.Name)
+                    .AppendLine(shippingInfo.Line1)
+                    .AppendLine(shippingInfo.Line2 ?? "")
+                    .AppendLine(shippingInfo.Line3 ?? "")
+                    .AppendLine(shippingInfo.City)
+                    .AppendLine(shippingInfo.State ?? "")
+                    .AppendLine(shippingInfo.Country)
+                    .AppendLine(shippingInfo.Zip)
+                    .AppendLine("---")
+                    .AppendFormat("Gift wrap: {0}", shippingInfo.GiftWrap ? "Yes" : "No");
+
+                MailMessage mailMessage = new MailMessage(
+                    this._emailSettings.MailFromAddress,    // From
+                    this._emailSettings.MailToAddress,      // To
+                    "New order submitted!",                 // Subject
+                    body.ToString());                       // Body
+
+                if (this._emailSettings.WriteAsFile)
+                {
+                    mailMessage.BodyEncoding = Encoding.ASCII;
+                }
+                smtpClient.Send(mailMessage);
+            }
+        }
+
+    }
+}
