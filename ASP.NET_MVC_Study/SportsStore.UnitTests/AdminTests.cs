b6ac7e1201947b0a5886d0cc3a71f4b84a9b0481
KeSaga <beijifeng_saga@163.com>
2015-08-20 09:55:22 +0800

    添加分类管理
    1、创建 CRUD 控制器
    2、创建新布局

---
+++ ASP.NET_MVC_Study/SportsStore.UnitTests/AdminTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void Index_Contains_All_Producs()
+        {
+            // 准备——创建模仿存储库
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(
+                new Product[]
+                {
+                    new Product{ProductID=1,Name="P1"},
+                    new Product{ProductID=1,Name="P1"},
+                    new Product{ProductID=1,Name="P1"}
+                }.AsQueryable());
+
+            // 准备——创建控制器
+            AdminController target = new AdminController(mock.Object);
+
+            // 动作
+            Product[] result = ((IEnumerable<Product>)target.Index().ViewData.Model).ToArray();
+
+            // 断言
+            Assert.AreEqual(result.Length, 3);
+            Assert.AreEqual("P1", result[0].Name);
+            Assert.AreEqual("P2", result[1].Name);
+            Assert.AreEqual("P3", result[2].Name);
+
+        }
+
+    }
+}
---
+++ ASP.NET_MVC_Study/SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository _repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            this._repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(this._repository.Products);
+        }
+
+    }
+}
