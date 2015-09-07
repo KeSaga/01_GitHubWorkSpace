8d2a65822273fb94783a47f4a70b9d71ca825739
KeSaga <beijifeng_saga@163.com>
2015-09-07 16:44:51 +0800

    完成简单路由的注册的介绍，并增加路由的测试
    路由测试的介绍待续...

---
+++ ASP.NET_MVC_Study/UrlsAndRoutes.Tests/RouteTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web;
+using System.Web.Routing;
+using Moq;
+using System.Reflection;
+
+namespace UrlsAndRoutes.Tests
+{
+    [TestClass]
+    public class RouteTests
+    {
+        [TestMethod]
+        private HttpContextBase CreateHttpContext(string targetUrl = null, string httpMethod = "GET")
+        {
+            // 创建模仿请求
+            Mock<HttpRequestBase> mockRequest = new Mock<HttpRequestBase>();
+            mockRequest.Setup(m => m.AppRelativeCurrentExecutionFilePath).Returns(targetUrl);
+            mockRequest.Setup(m => m.HttpMethod).Returns(httpMethod);
+
+            // 创建模仿响应
+            Mock<HttpResponseBase> mockResponse = new Mock<HttpResponseBase>();
+            mockResponse.Setup(m => m.ApplyAppPathModifier(It.IsAny<string>())).Returns<string>(s => s);
+
+            // 创建使用上述请求和响应的模仿上下文
+            Mock<HttpContextBase> mockContext = new Mock<HttpContextBase>();
+            mockContext.Setup(m => m.Request).Returns(mockRequest.Object);
+            mockContext.Setup(m => m.Response).Returns(mockResponse.Object);
+
+            // 返回模仿的上下文
+            return mockContext.Object;
+
+        }
+
+    }
+}
--- ASP.NET_MVC_Study/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ ASP.NET_MVC_Study/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -18,6 +18,14 @@ namespace UrlsAndRoutes
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            // 通过创建一个新的 Route 实现路由的注册
+            Route myRoute = new Route("{controller}/{action}", new MvcRouteHandler());
+            routes.Add("MyRoute", myRoute);
+
+            // 使用 RouteCollection 类所定义的MapRoute方法实现路由的注册（效果与上面方式相同）
+            routes.MapRoute("MyRoute", "{controller}/{action}");
+
         }
     }
 }
