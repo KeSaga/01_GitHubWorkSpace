bcfaae53955d94f448c6a3d59d0859d36fffea8d
KeSaga <beijifeng_saga@163.com>
2015-12-07 23:20:34 +0800

    介绍定制路由系统——1、创建自定义的RouteBase实现（未完）

---
+++ ASP.NET_MVC_Study/UrlsAndRoutes/Controllers/LegacyController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    /// <summary>
+    /// 用以处理旧式 URL 请求的控制器
+    /// </summary>
+    public class LegacyController : Controller
+    {
+
+        public ActionResult GetLegacyURL(string legacyURL)
+        {
+            // 应用程序迁移到 MVC 之前，请求是针对文件的，因此，实际上是需要在这里处理被请求的文件。但这里
+            // 只简单说明一下自定义 RouteBase 的实现原理，所以，此处仅在视图中显示这个 URL。
+            return View((object)legacyURL);
+        }
+
+    }
+}
