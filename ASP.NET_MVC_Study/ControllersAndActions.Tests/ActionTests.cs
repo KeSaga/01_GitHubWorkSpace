e40dae105eb7fc6f682c503a81c75a3d2b9a8803
KeSaga <beijifeng_saga@163.com>
2016-01-07 23:29:07 +0800

    介绍产生输出——通过渲染视图返回HTML

---
+++ ASP.NET_MVC_Study/ControllersAndActions.Tests/ActionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControllersAndActions.Controllers;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Tests
+{
+    [TestClass]
+    public class ActionTests
+    {
+        [TestMethod]
+        public void ViewSelectionTest()
+        {
+            // 准备——创建控制器
+            ExampleController target = new ExampleController();
+            // 动作——调用动作方法
+            ViewResult result = target.Index();
+            // 断言——检测结果
+            Assert.AreEqual("Homepage", result.ViewName);
+        }
+    }
+}
---
+++ ASP.NET_MVC_Study/ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            return View("Homepage");
+        }
+
+        ///// <summary>
+        ///// 通过路径指定要渲染的视图（一般建议最好使用控制器的动作方法来定向）
+        ///// </summary>
+        ///// <returns></returns>
+        //public ViewResult Index()
+        //{
+        //    return View("~/Views/Other/Index.cshtml");
+        //}
+
+    }
+}
