2fc6c67435784a73c982936ae403268709fba67b
KeSaga <beijifeng_saga@163.com>
2016-01-31 22:20:51 +0800

    创建一个自定义的认证过滤器类

---
+++ ASP.NET_MVC_Study/Filters/Infrastructure/CustomAuthAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+    public class CustomAuthAttribute : AuthorizeAttribute
+    {
+        private bool _localAllowed;
+
+        public CustomAuthAttribute(bool allowedParam)
+        {
+            this._localAllowed = allowedParam;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Request.IsLocal)
+            {
+                return this._localAllowed;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
