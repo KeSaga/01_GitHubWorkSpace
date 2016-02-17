2d09eb5bc7c13cb2099569eec1d78bfc25500a93
KeSaga <beijifeng_saga@163.com>
2016-02-17 09:55:56 +0800

    介绍如何使用异常过滤器——未完，待续

---
+++ ASP.NET_MVC_Study/Filters/Infrastructure/RangeExceptionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+    public class RangeExceptionAttribute : FilterAttribute, IExceptionFilter
+    {
+
+        public void OnException(ExceptionContext filterContext)
+        {
+
+            if (!filterContext.ExceptionHandled &&
+                filterContext.Exception is ArgumentOutOfRangeException)
+            {
+                filterContext.Result = new RedirectResult("~/Content/RangerErrorPage.html");
+                filterContext.ExceptionHandled = true;
+            }
+        }
+    }
+}
