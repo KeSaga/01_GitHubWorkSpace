c3dde4d8ee3c6ed990a0d5a82f7f7c42be43c145
KeSaga <beijifeng_saga@163.com>
2016-02-24 11:05:08 +0800

    接收控制器的可扩展性
    准备项目、创建自定义控制器工厂（未完）

---
+++ ASP.NET_MVC_Study/ControllerExtensibility/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using ControllerExtensibility.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            return View("Result", new Result { ControllerName = "Customer", ActionName = "Index" });
+        }
+
+        public ViewResult List()
+        {
+            return View("Result", new Result { ControllerName = "Customer", ActionName = "List" });
+        }
+
+    }
+}
