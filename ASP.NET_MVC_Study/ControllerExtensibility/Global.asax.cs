25173b98a4e4f5551ebee675977b79acbb4a76ae
KeSaga <beijifeng_saga@163.com>
2016-02-29 10:25:35 +0800

    完成自定义控制器工厂的介绍

--- ASP.NET_MVC_Study/ControllerExtensibility/Global.asax.cs
+++ ASP.NET_MVC_Study/ControllerExtensibility/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ControllerExtensibility.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,6 +20,8 @@ namespace ControllerExtensibility
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ControllerBuilder.Current.SetControllerFactory(new CustomControllerFactory());
         }
     }
 }
