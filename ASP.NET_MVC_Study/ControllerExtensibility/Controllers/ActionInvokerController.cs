4a3177e47a8f4c51f36b55a00de8c263499a4c9d
KeSaga <beijifeng_saga@163.com>
2016-03-02 10:23:54 +0800

    介绍如何创建自定义动作调用器

---
+++ ASP.NET_MVC_Study/ControllerExtensibility/Controllers/ActionInvokerController.cs
@@ -0,0 +1,21 @@
+﻿using ControllerExtensibility.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class ActionInvokerController : Controller
+    {
+        //
+        // GET: /ActionInvoker/
+
+        public ActionInvokerController()
+        {
+            this.ActionInvoker = new CustomActionInvoker();
+        }
+
+    }
+}
