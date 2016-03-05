a3b6daed8bbc3e55021468cd5086744e13111b70
KeSaga <beijifeng_saga@163.com>
2016-03-05 11:25:36 +0800

    使用内建的动作调用器
    使用动作方法选择——创建自定义动作方法选择器

---
+++ ASP.NET_MVC_Study/ControllerExtensibility/Infrastructure/LocalAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ControllerExtensibility.Infrastructure
+{
+    public class LocalAttribute : ActionMethodSelectorAttribute
+    {
+
+        public override bool IsValidForRequest(ControllerContext controllerContext, System.Reflection.MethodInfo methodInfo)
+        {
+            return controllerContext.HttpContext.Request.IsLocal;
+        }
+    }
+}
