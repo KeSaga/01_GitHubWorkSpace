a29524e2f37eb0785cfd4aa6085a1f403d4a9547
KeSaga <beijifeng_saga@163.com>
2016-03-09 10:02:23 +0800

    视图
    创建自定义视图引擎：创建示例项目

---
+++ ASP.NET_MVC_Study/Views/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Hello, World";
+            ViewData["Time"] = DateTime.Now.ToShortTimeString();
+
+            return View("DebugData");
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+    }
+}
