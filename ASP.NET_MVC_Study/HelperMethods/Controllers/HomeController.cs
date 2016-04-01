794f03cb1fd9968f0667caadb5cdf8810d408deb
KeSaga <beijifeng_saga@163.com>
2016-04-01 10:45:52 +0800

    辅助器方法
    创建自定义辅助器方法:
    1、内联辅助器方法
    2、外部辅助器方法

---
+++ ASP.NET_MVC_Study/HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new string[] { "Apple", "Orange", "Pear" };
+            ViewBag.Cities = new string[] { "New York", "London", "Paris" };
+
+            string message = "This is an HTML element: <input>";
+
+            return View((object)message);
+        }
+
+    }
+}
---
+++ ASP.NET_MVC_Study/HelperMethods/Infrastructure/CustomHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelperMethods.Infrastructure
+{
+    public static class CustomHelpers
+    {
+
+        public static MvcHtmlString ListArrayItems(this HtmlHelper html, string[] list)
+        {
+            TagBuilder tag = new TagBuilder("ul");
+
+            foreach (string str in list)
+            {
+                TagBuilder itemTag = new TagBuilder("li");
+                itemTag.SetInnerText(str);
+                tag.InnerHtml += itemTag.ToString();
+            }
+
+            return new MvcHtmlString(tag.ToString());
+        }
+
+    }
+}
