9f774761a3881443dee3c34267b9e8c90350fc3f
KeSaga <beijifeng_saga@163.com>
2016-04-27 17:29:45 +0800

    使用模板辅助器方法

--- ASP.NET_MVC_Study/HelperMethods/Controllers/HomeController.cs
+++ ASP.NET_MVC_Study/HelperMethods/Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace HelperMethods.Controllers
         [HttpPost]
         public ActionResult CreatePerson(Person person)
         {
-            return View(person);
+            return View("DisplayPerson", person);
         }
 
     }
