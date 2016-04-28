014ca8a32fbe37cd32c48b4fce66ea08a3f3a456
KeSaga <beijifeng_saga@163.com>
2016-04-28 17:34:41 +0800

    使用模型元数据
    1、用元数据控制编辑及可见性
    2、使用用于标签的元数据
    3、使用用于数据值的元数据

--- ASP.NET_MVC_Study/HelperMethods/Models/Person.cs
+++ ASP.NET_MVC_Study/HelperMethods/Models/Person.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace HelperMethods.Models
 {
+    // DisplayName 也可以用于属性，但习惯上建议在模型类上应用，而在属性上使用 Display
+    [DisplayName("New Person")]
     public class Person
     {
+        [HiddenInput(DisplayValue = false)]
         public int PersonId { get; set; }
+        [Display(Name = "First")]
         public string FirstName { get; set; }
+        [Display(Name = "Last")]
         public string LastName { get; set; }
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
         public Address HomeAddress { get; set; }
+        [Display(Name = "Approved")]
         public bool IsApproved { get; set; }
         public Role Role { get; set; }
     }
