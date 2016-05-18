ef446b68c17822e20bb51b1a6f5cbd82b73ba849
KeSaga <beijifeng_saga@163.com>
2016-05-18 17:35:24 +0800

    模型绑定
    1、创建项目
    2、理解模型绑定（待续...）

---
+++ ASP.NET_MVC_Study/MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using MvcModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Person[] personData =
+        {
+            new Person{PersonId = 1, FirstName = "Adam",LastName = "Freeman",Role = Role.Admin},
+            new Person{PersonId = 2 ,FirstName = "Steven",LastName = "Sanderson",Role = Role.Admin},
+            new Person{PersonId = 3 ,FirstName = "Jacqui",LastName = "Griffyth",Role = Role.User},
+            new Person{PersonId = 4 ,FirstName = "John",LastName = "Smith",Role = Role.User},
+            new Person{PersonId = 5 ,FirstName = "Anne",LastName = "Jones",Role = Role.Guest}
+        };
+
+        public ActionResult Index(int id)
+        {
+            Person dataItem = personData.Where(p => p.PersonId == id).First();
+            return View(dataItem);
+        }
+
+    }
+}
