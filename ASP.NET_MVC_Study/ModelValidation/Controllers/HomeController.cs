efdfbf5e5c6c28291ee13510c27c24fd105d249a
KeSaga <beijifeng_saga@163.com>
2016-05-23 18:04:06 +0800

    模型验证
    1、示例项目介绍
    2、明确地验证模型

---
+++ ASP.NET_MVC_Study/ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using ModelValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment { Date = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            if (string.IsNullOrEmpty(appt.ClinetName))
+            {
+                ModelState.AddModelError("ClinetName", "Please enter your name");
+            }
+
+            if (ModelState.IsValidField("Date") && DateTime.Now > appt.Date)
+            {
+                ModelState.AddModelError("Date", "Please enter a date in the future");
+            }
+
+            if (!appt.TermsAccepted)
+            {
+                ModelState.AddModelError("TermsAccepted", "You must accept the terms");
+            }
+
+            if (ModelState.IsValid)
+            {
+                // 在实际项目中，此处是存储库中存储新的 Appointment 的语句
+                return View("Completed", appt);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+    }
+}
---
+++ ASP.NET_MVC_Study/ModelValidation/Models/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelValidation.Models
+{
+    public class Appointment
+    {
+
+        public string ClinetName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public bool TermsAccepted { get; set; }
+
+    }
+}
