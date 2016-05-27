7627171b34964515d9e5a5c189e8ca44bb682c7c
KeSaga <beijifeng_saga@163.com>
2016-05-27 17:28:03 +0800

    捆绑包与显示模式
    1、理解默认脚本库
    2、准备示例
    3、管理脚本与样式表
    4、使用脚本和样式捆绑包（未完，待续...）

---
+++ ASP.NET_MVC_Study/ClientFeatures/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ClientFeatures
+{
+    public class BundleConfig
+    {
+        // 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/*.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/clientfeaturesscripts").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.validate.js",
+                        "~/Scripts/jquery.validate.unobtrusive.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+
+        }
+    }
+}
---
+++ ASP.NET_MVC_Study/ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using ClientFeatures.Models;
+using System;
+using System.Web.Mvc;
+
+namespace ClientFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment
+            {
+                ClientName = "Adam",
+                Date = DateTime.Now.AddDays(2),
+                TermsAccepted = true
+            });
+        }
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appt)
+        {
+            // 在实际项目中，这里是存储新 Appointment 的语句
+            return Json(appt, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
