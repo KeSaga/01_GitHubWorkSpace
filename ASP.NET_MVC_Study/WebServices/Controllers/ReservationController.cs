4b39276e8aa35d2911d09a88d38ed1011ba8ad41
KeSaga <beijifeng_saga@163.com>
2016-05-31 17:26:23 +0800

    完成对Web API的介绍

---
+++ ASP.NET_MVC_Study/WebServices/Controllers/ReservationController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class ReservationController : ApiController
+    {
+        IReservationRepository repo = ReservationRepository.getRepository();
+
+        public IEnumerable<Reservation> GetAllReservation()
+        {
+            return repo.GetAll();
+        }
+
+        public Reservation GetReservation(int id)
+        {
+            return repo.Get(id);
+        }
+
+        [HttpPost]
+        public Reservation CreateReservation(Reservation item)
+        {
+            return repo.Add(item);
+        }
+
+        [HttpPut]
+        public bool UpdateReservation(Reservation item)
+        {
+            return repo.Update(item);
+        }
+
+        public void DeleteReservation(int id)
+        {
+            repo.Remove(id);
+        }
+
+    }
+}
