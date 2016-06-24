0af3e38d72c9faf1dd8824f7f71984143add2461
KeSaga <beijifeng_saga@163.com>
2016-06-24 19:58:51 +0800

    更新

---
+++ ASP.NET_Technology/ASP.NET_Technology/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP.NET_Technology.Models;
+
+namespace ASP.NET_Technology.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public JsonResult Index()
+        {
+            LyfItemVM itmVM = new LyfItemVM
+                {
+                    ItemName = "Saga"
+                };
+
+            return Json(itmVM);
+        }
+
+        public ViewResult TestView()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ASP.NET_Technology/ASP.NET_Technology/Models/LyfItemVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_Technology.Models
+{
+    /// <summary>
+    /// 项目对象的视图模型
+    /// </summary>
+    public class LyfItemVM
+    {
+
+        public string ItemName { get; set; }
+
+        public string Detail { get; set; }
+
+        public string Example { get; set; }
+
+        //public object Preview { get; set; }
+
+    }
+}
