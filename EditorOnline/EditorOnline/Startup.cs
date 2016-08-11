1a55408b2af2c5eadbe3089351e7713b7fe91afe
KeSaga <beijifeng_saga@163.com>
2016-08-11 17:25:02 +0800

    新建

---
+++ EditorOnline/EditorOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EditorOnline.Startup))]
+namespace EditorOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
