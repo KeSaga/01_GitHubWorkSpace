68054ab734c96b99b0095a6f5185e512e90008b4
Administrator <Administrator@COS-20160816ZCF>
2016-08-26 17:41:32 +0800

    创建自己的绘图通用库

---
+++ Lyf.DrawingLibrary/Lyf.DrawingLibrary/2D/Border.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyf.DrawingLibrary._2D
+{
+    /// <summary>
+    /// 图形对象边框类
+    /// 功能：实现用于绘制各种图表对象边框的功能
+    /// </summary>
+    [Serializable]
+    public class Border : LineBase, ISerializable, ICloneable
+    {
+        #region 常量
+
+        public const int schema = 11;
+
+
+        #endregion
+
+        #region 构造函数
+
+        /// <summary>
+        /// 实现拷贝
+        /// </summary>
+        /// <param name="rhs"></param>
+        public Border(Border lBase) : base(lBase)
+        {
+        }
+
+        #endregion
+
+        #region 函数
+
+        public Border Clone()
+        {
+            return new Border(this);
+        }
+
+        #endregion
+
+
+        #region ISerializable 接口实现
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            info.AddValue("schema", schema);
+        }
+
+        #endregion
+
+        #region ICloneable 接口实现
+
+        object ICloneable.Clone()
+        {
+            return this.Clone();
+        }
+
+        #endregion
+
+    }
+}
---
+++ Lyf.DrawingLibrary/Lyf.DrawingLibrary/2D/LineBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyf.DrawingLibrary._2D
+{
+    /// <summary>
+    /// 定义线段的基本属性
+    /// </summary>
+    [Serializable]
+    public class LineBase : ISerializable, ICloneable
+    {
+        public LineBase(LineBase lBase)
+        {
+
+        }
+
+        [SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
+        public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+
+        }
+
+        object ICloneable.Clone()
+        {
+            throw new NotImplementedException("Can't clone an abstract base type -- child types must implement ICloneable");
+        }
+
+    }
+}
